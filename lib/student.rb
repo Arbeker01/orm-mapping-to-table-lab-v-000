68eff3094a458a4d7bdf7abc4f52bdff6daccd5a
Romeo Gono <romeogono1@aol.com>
2018-08-14 23:57:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,26 @@
 class Student
+  attr_accessor :id, :name, :grade
 
+  def initialize(:name, :grade, :id=nil)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end
+
+  def self.create_table 
+    
+  end
+
+  def self.drop_table
+    
+  end
+
+  def save
+  end
+
+  def self.create(:name, :grade)
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
